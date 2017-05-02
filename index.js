cd5338fb2ae99ea2607804b9ac19573f5b418d2d
Graham Wood <graham@MacBook-Pro-2.local>
2017-05-02 10:59:20 -0500

    Added connection to database.

--- index.js
+++ index.js
@@ -3,13 +3,27 @@
 require('dotenv').config();
 const express = require('express');
 const chalk = require('chalk');
+const {
+    MongoClient
+} = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 80;
+const dbAddress = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@ds129031.mlab.com:29031/fcc-image-search`;
 
 app.get('*', (req, res) => {
-  res.end("Hello");
+  MongoClient.connect(dbAddress, (err, db) => {
+    if (err) {
+      console.log(err);
+    } else {
+      
+    }
+  });
+
+  res.end("TODO: Show homepage and some recent results.");
 });
+
+
 
 app.listen(port, () => {
     console.log(chalk.yellow(`Listening on port: ${port}`));
