f08fe59e993b223e854c944a3090a98c3312d57d
Graham Wood <graham@MacBook-Pro-2.local>
2017-05-02 13:12:09 -0500

    Scaffolded routes.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
+const router = express.Router();
 const chalk = require('chalk');
 const {
     MongoClient
@@ -11,19 +12,27 @@ const app = express();
 const port = process.env.PORT || 80;
 const dbAddress = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@ds129031.mlab.com:29031/fcc-image-search`;
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   MongoClient.connect(dbAddress, (err, db) => {
     if (err) {
       console.log(err);
     } else {
-      
+
     }
   });
 
   res.end("TODO: Show homepage and some recent results.");
 });
 
+app.get('/api/imagesearch/:str', (req, res) => {
+  res.end("test");
+  console.log(req.params);
+  console.log(req.query);
+});
 
+app.get('/api/latest/imagesearch/', (req, res) => {
+  res.end("TODO: display image search lastest requests.");
+})
 
 app.listen(port, () => {
     console.log(chalk.yellow(`Listening on port: ${port}`));
