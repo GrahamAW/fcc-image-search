bc3a8a684fc6459576adbabd4c1200ef3297989f
Graham Wood <graham@MacBook-Pro-2.local>
2017-05-03 17:27:56 -0500

    Added the google-search package and setup seaching.

--- index.js
+++ index.js
@@ -12,80 +12,107 @@ const moment = require('moment');
 const app = express();
 const hbs = require('hbs');
 
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+    key: 'AIzaSyBIpLZeNNcMHquhYO7Ot4-SEg1PUJuFA9c',
+    cx: '012081856853641415610:a4c89_xsdqc'
+});
+
 app.set('view engine', 'hbs');
 
 const port = process.env.PORT || 80;
 const dbAddress = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@ds129031.mlab.com:29031/fcc-image-search`;
 
 app.get('/', (req, res) => {
-  MongoClient.connect(dbAddress, (err, db) => {
-    if (err) {
-      console.log(err);
-    } else {
-      //TODO: pass recent searches
-      res.render('index.hbs');
-    }
-  });
+    MongoClient.connect(dbAddress, (err, db) => {
+        if (err) {
+            console.log(err);
+        } else {
+            //TODO: pass recent searches
+            res.render('index.hbs');
+        }
+    });
 });
 
 app.get('/api/imagesearch/:str', (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
+    // save to database
+    MongoClient.connect(dbAddress, (err, db) => {
+        if (err) {
+            console.log(err);
+        } else {
 
-  // save to database
-  MongoClient.connect(dbAddress, (err, db) => {
-    if (err) {
-      console.log(err);
-    } else {
+            const formattedSearch = {
+                'search_string': req.params.str || 'problem',
+                'timestamp': parseInt(moment().format('x'))
+            };
 
-      const formattedSearch = {
-        'search_string' : req.params.str || 'problem',
-        'timestamp' : parseInt(moment().format('x'))
-      };
-
-      db.collection('searchs').insertOne(formattedSearch).then((doc) => {
-        console.log(doc.ops);
-      });
-    }
-  });
+            db.collection('searchs').insertOne(formattedSearch).then((doc) => {
+                // console.log(doc.ops);
+            });
+        }
+    });
 
 
+    // get and display results
+    googleSearch.build({
+        q: req.params.str,
+        start: (parseInt(req.query.offset) || 1),
+        searchType: 'image',
+        num: 10, // Number of search results to return between 1 and 10, inclusive
+        // siteSearch: "http://kitaplar.ankara.edu.tr/" // Restricts results to URLs from a specified site
+    }, function(error, response) {
+        if (response.hasOwnProperty('items') && response.items.length) {
 
-  // get and display results
-  const result = {
-    'url' : 'test',
-    'snippest' : 'test',
-    'thumbnail' : 'test',
-    'context' : 'test'
-  };
-  res.send(result);
+            let results = [];
+
+            for (let i = 0; i < response.items.length; i++) {
+                const result = {
+                    'url': response.items[i].link,
+                    'snippest': response.items[i].snippet
+                        .slice(0, response.items[i].snippet.indexOf('\n')),
+                    'thumbnail': response.items[i].link,
+                    'context': response.items[i].displayLink
+                };
+                results.push(result);
+            }
+
+            res.send(results);
+        } else {
+            res.end('no results');
+        }
+    });
+
+
+    // res.send(result);
 });
 
 app.get('/api/latest/imagesearch/', (req, res) => {
-  MongoClient.connect(dbAddress, (err, db) => {
-    if (err) {
-      console.log(err);
-    } else {
-      db.collection('searchs').find()
-        .sort({'timestamp': -1})
-        .limit(10)
-        .toArray()
-        .then((doc) => {
+    MongoClient.connect(dbAddress, (err, db) => {
+        if (err) {
+            console.log(err);
+        } else {
+            db.collection('searchs').find()
+                .sort({
+                    'timestamp': -1
+                })
+                .limit(10)
+                .toArray()
+                .then((doc) => {
 
-        let results = [];
+                    let results = [];
 
-        for (let i = 0; i < doc.length; i++) {
-          console.log(doc[i]);
-          results.push({
-            'search_string' : doc[i].search_string,
-            'timestamp' : moment(doc[i].timestamp).format()
-          });
+                    for (let i = 0; i < doc.length; i++) {
+                        console.log(doc[i]);
+                        results.push({
+                            'search_string': doc[i].search_string,
+                            'timestamp': moment(doc[i].timestamp).format()
+                        });
+                    }
+                    // console.log(doc.length);
+                    res.send(results);
+                });
         }
-        console.log(doc.length);
-        res.send(results);
-      });
-    }
-  });
+    });
 });
 
 app.listen(port, () => {
